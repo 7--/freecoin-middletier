273fc63f8e74c35fca66b2f13e6311b8cc52aaf0
Phil <regop412@gmail.com>
2018-03-27 12:43:45 -0400

    add spring dev tools. remove and rename quiz and question endpoints.

--- src/main/java/tk/freecoin/controller/QuestionsController.java
+++ src/main/java/tk/freecoin/controller/QuestionsController.java
@@ -17,76 +17,10 @@ public class QuestionsController {
 	@Autowired
 	QuestionsDao questionsDao;
 
-	@GetMapping(value = "/questions", produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Questions> getQuestions() {
-		List<Questions> Questions = questionsDao.findAll();
-		return Questions;
-	}
-
 	@GetMapping(value = "/question", produces = MediaType.APPLICATION_JSON_VALUE)
-	public Questions getQuestion(@RequestParam(value = "questionid", required = true) int questionid) {
-		Questions question = questionsDao.findByQuestionid(questionid);
-		return question;
-	}
-
-	@GetMapping(value = "/questionslist", produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Questions> getQuestionsByQuiz(@RequestParam(value = "quiz", required = true) int quizid) {
+	public List<Questions> getQuestionsByQuiz(@RequestParam(value = "quizid", required = true) Integer quizid) {
 		List<Questions> questions = questionsDao.findByQuiz(quizid);
-
 		return questions;
 	}
 
-	/*
-	 * @GetMapping(value =
-	 * "/addQuestions/{quiz}/{question}/{choice1}/{choice2}/{choice3}/{choice4}/{answer}/",
-	 * produces = MediaType.APPLICATION_JSON_VALUE) public void
-	 * addQuestions(@PathVariable("quiz") int quiz,
-	 * 
-	 * @PathVariable("question") String question,
-	 * 
-	 * @PathVariable("choice1") String choice1,
-	 * 
-	 * @PathVariable("choice2") String choice2,
-	 * 
-	 * @PathVariable("choice3") String choice3,
-	 * 
-	 * @PathVariable("choice4") String choice4,
-	 * 
-	 * @PathVariable("answer") int answer) {
-	 * 
-	 * Questions questions = new Questions(quiz, question, choice1, choice2,
-	 * choice3, choice4, answer); questionsDao.save(questions); }
-	 * 
-	 * @GetMapping(value =
-	 * "/updateQuestions/{questions_id}/{quiz}/{question}/{choice1}/{choice2}/{choice3}/{choice4}/{answer}/",
-	 * produces = MediaType.APPLICATION_JSON_VALUE) public void
-	 * update_Questions(@PathVariable("questions_id") int questions_id,
-	 * 
-	 * @PathVariable("quiz") int quiz,
-	 * 
-	 * @PathVariable("question") String question,
-	 * 
-	 * @PathVariable("choice1") String choice1,
-	 * 
-	 * @PathVariable("choice2") String choice2,
-	 * 
-	 * @PathVariable("choice3") String choice3,
-	 * 
-	 * @PathVariable("choice4") String choice4,
-	 * 
-	 * @PathVariable("answer") int answer) {
-	 * 
-	 * Questions questions = questionsDao.findByQuestionsid(questions_id);
-	 * questions.setQuiz(quiz); questions.setQuestion(question);
-	 * questions.setChoice1(choice1); questions.setChoice2(choice2);
-	 * questions.setChoice3(choice3); questions.setChoice4(choice4);
-	 * questions.setAnswer(answer); questionsDao.save(questions); }
-	 * 
-	 * @GetMapping(value = "/deleteQuestions/{questions_id}/", produces =
-	 * MediaType.APPLICATION_JSON_VALUE) public void
-	 * delete_Questions(@PathVariable("questions_id") int questions_id) {
-	 * 
-	 * Questions questions = questionsDao.findByQuestionsid(questions_id);
-	 * questionsDao.delete(questions); }
-	 */
 }
--- src/main/java/tk/freecoin/controller/QuizController.java
+++ src/main/java/tk/freecoin/controller/QuizController.java
@@ -17,40 +17,15 @@ public class QuizController {
 	@Autowired
 	QuizDao quizDao;
 
-	@GetMapping(value = "/quizes", produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Quiz> getQuizes() {
-		List<Quiz> quizes = quizDao.findAll();
-		return quizes;
-	}
-
 	@GetMapping(value = "/quiz", produces = MediaType.APPLICATION_JSON_VALUE)
-	public Quiz getQuiz(@RequestParam(value = "quizid", required = true) int quizid) {
-		Quiz quiz = quizDao.findByQuizid(quizid);
-		return quiz;
+	public Object getQuiz(@RequestParam(value = "id", required = false) Integer quizid) {
+		if (quizid == null) {
+			List<Quiz> quizes = quizDao.findAll();
+			return quizes;
+		} else {
+			Quiz quiz = quizDao.findByQuizid(quizid);
+			return quiz;
+		}
 	}
 
-	/*
-	 * @GetMapping(value = "/addQuiz/{name}/", produces =
-	 * MediaType.APPLICATION_JSON_VALUE) public void
-	 * addQuiz(@PathVariable("name") String name) {
-	 * 
-	 * Quiz quiz = new Quiz(name); quizDao.save(quiz); }
-	 * 
-	 * 
-	 * @GetMapping(value =
-	 * "/updateQuiz/{quiz_id}/{email}/{password}/{correctAnswer}/{wrongAnswer}/{fname}/{lname}/",
-	 * produces = MediaType.APPLICATION_JSON_VALUE) public void
-	 * update_quiz(@PathVariable("quiz_id") int quiz_id,
-	 * 
-	 * @PathVariable("name") String name) {
-	 * 
-	 * Quiz quiz = quizDao.findByQuizid(quiz_id); quiz.setName(name);
-	 * quizDao.save(quiz); }
-	 * 
-	 * @GetMapping(value = "/deleteQuiz/{quiz_id}/", produces =
-	 * MediaType.APPLICATION_JSON_VALUE) public void
-	 * delete_quiz(@PathVariable("quiz_id") int quiz_id) {
-	 * 
-	 * Quiz quiz = quizDao.findByQuizid(quiz_id); quizDao.delete(quiz); }
-	 */
 }
